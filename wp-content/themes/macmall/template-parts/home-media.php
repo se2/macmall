b1f91c1f8eb2cbe7669dfe5b8cf84f5942fdc0fc
Tu Luong <cesctom0401@gmail.com>
2019-01-14 23:03:11 -0500

    Update home media ACF, default to desktop if tablet or mobile not set

--- wp-content/themes/macmall/template-parts/home-media.php
+++ wp-content/themes/macmall/template-parts/home-media.php
@@ -11,13 +11,15 @@
 if ( have_rows( 'home_media' ) ) :
 	while ( have_rows( 'home_media' ) ) :
 		the_row();
-		$link = get_sub_field( 'media_link' );
+		$link   = get_sub_field( 'media_link' );
+		$tablet = get_sub_field( 'tablet_image' ) ? get_sub_field( 'tablet_image' ) : get_sub_field( 'desktop_image' );
+		$mobile = get_sub_field( 'mobile_image' ) ? get_sub_field( 'mobile_image' ) : $tablet;
 ?>
 	<section class="home-media">
 		<a href="<?php echo esc_url( $link['url'] ); ?>" target="<?php echo esc_attr( $link['target'] ); ?>">
 			<img src="<?php the_sub_field( 'desktop_image' ); ?>" alt="" class="w-full hidden xl:block desktop-image">
-			<img src="<?php the_sub_field( 'tablet_image' ); ?>" alt="" class="w-full hidden md:block xl:hidden tablet-image">
-			<img src="<?php the_sub_field( 'mobile_image' ); ?>" alt="" class="w-full block md:hidden mobile-image">
+			<img src="<?php echo esc_url( $tablet ); ?>" alt="" class="w-full hidden md:block xl:hidden tablet-image">
+			<img src="<?php echo esc_url( $mobile ); ?>" alt="" class="w-full block md:hidden mobile-image">
 		</a>
 		<?php if ( get_sub_field( 'bottom_divider' ) ) : ?>
 		<div class="bottom-divider w-full h-12 block md:hidden" style="background-color:<?php the_sub_field( 'bottom_divider_color' ); ?>;"></div>
