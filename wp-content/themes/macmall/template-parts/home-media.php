009779ae9157c4816de2f437d380959ba244c68f
Tu Luong <cesctom0401@gmail.com>
2019-02-18 17:20:46 +0700

    Fix bug cannot scroll menu mobile

--- wp-content/themes/macmall/template-parts/home-media.php
+++ wp-content/themes/macmall/template-parts/home-media.php
@@ -15,18 +15,18 @@ if ( have_rows( 'home_media' ) ) :
 		$tablet = get_sub_field( 'tablet_image' ) ? get_sub_field( 'tablet_image' ) : get_sub_field( 'desktop_image' );
 		$mobile = get_sub_field( 'mobile_image' ) ? get_sub_field( 'mobile_image' ) : $tablet;
 ?>
-	<section class="home-media flex flex-wrap overflow-x-hidden py-5">
-		<a href="<?php echo esc_url( $link['url'] ); ?>" target="<?php echo esc_attr( $link['target'] ); ?>" class="block w-full lg:<?php the_sub_field( 'layout' ); ?> <?php echo ( get_sub_field( 'layout' ) == 'w-1/2' ) ? 'lg:pr-5' : ''; ?>">
-			<img src="<?php the_sub_field( 'desktop_image' ); ?>" alt="" class="w-full hidden xl:block desktop-image">
-			<img src="<?php echo esc_url( $tablet ); ?>" alt="" class="w-full hidden md:block xl:hidden tablet-image">
-			<img src="<?php echo esc_url( $mobile ); ?>" alt="" class="w-full block md:hidden mobile-image">
+	<section class="home-media flex flex-wrap overflow-x-hidden">
+		<a href="<?php echo esc_url( $link['url'] ); ?>" target="<?php echo esc_attr( $link['target'] ); ?>" class="block w-full lg:<?php the_sub_field( 'layout' ); ?> <?php echo ( get_sub_field( 'layout' ) == 'w-1/2' ) ? 'lg:pr-6' : ''; ?> pb-12">
+			<img src="<?php the_sub_field( 'desktop_image' ); ?>" alt="" class="h-full w-full hidden xl:block desktop-image">
+			<img src="<?php echo esc_url( $tablet ); ?>" alt="" class="h-full w-full hidden md:block xl:hidden tablet-image">
+			<img src="<?php echo esc_url( $mobile ); ?>" alt="" class="h-full w-full block md:hidden mobile-image">
 		</a>
 		<?php
 		if ( ( get_sub_field( 'layout' ) == 'w-1/2' ) && ( get_sub_field( 'desktop_image_2' ) ) ) :
 			$link_2 = get_sub_field( 'media_link_2' );
 		?>
-		<a href="<?php echo esc_url( $link_2['url'] ); ?>" target="<?php echo esc_attr( $link_2['target'] ); ?>" class="block w-full lg:<?php the_sub_field( 'layout' ); ?> lg:pl-5">
-			<img src="<?php the_sub_field( 'desktop_image_2' ); ?>" alt="" class="w-full">
+		<a href="<?php echo esc_url( $link_2['url'] ); ?>" target="<?php echo esc_attr( $link_2['target'] ); ?>" class="block w-full lg:<?php the_sub_field( 'layout' ); ?> lg:pl-6 pb-12">
+			<img src="<?php the_sub_field( 'desktop_image_2' ); ?>" alt="" class="w-full h-full">
 		</a>
 		<?php endif; ?>
 	</section>
